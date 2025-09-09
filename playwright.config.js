1dcf8381f210d45cdc6cbe97688baff9d175e5a1
Viviane Hermano <vivianephnunes@gmail.com>
2025-09-09 11:36:38 -0300

    fix: update base files

--- playwright.config.js
+++ playwright.config.js
@@ -3,16 +3,51 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 2 : 4,
+  
+  reporter: [
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'relatorios/resultados.json' }],
+    ['list'],
+    ['github']
+  ],
+  
   use: {
     baseURL: 'https://www.saucedemo.com',
-    headless: true,
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+    
+    actionTimeout: 10000,
+    navigationTimeout: 30000,
   },
+  
   projects: [
     {
-      name: 'chromium',
-      use: { channel: 'chrome' }
+      name: 'chrome',
+      use: { 
+        channel: 'chrome',
+        viewport: { width: 1920, height: 1080 },
+        userAgent: 'Teste-Automatizado-Chrome'
+      }
+    },
+    {
+      name: 'firefox',
+      use: { 
+        browserName: 'firefox',
+        viewport: { width: 1920, height: 1080 }
+      }
+    },
+    {
+      name: 'mobile',
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 390, height: 844 },
+        userAgent: 'Teste-Mobile-iPhone'
+      }
     }
   ]
 });
